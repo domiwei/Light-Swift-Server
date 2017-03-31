ccc1350c94af79655173ec07fb8ca0d73fddb1dc
domiwei <kewei.train@gmail.com>
2017-04-01 04:14:12 +0800

    Add logger

--- swifttest/log.go
+++ swifttest/log.go
@@ -5,7 +5,38 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 )
+
+type ServerLogger struct {
+	filename string
+	out *os.File
+	logger *log.Logger
+}
+
+// Create logger
+func OpenLogger(filename string) *ServerLogger {
+	logFile, err := os.Create(filename)
+	if err != nil {
+		return nil
+	}
+
+	return &ServerLogger{
+		filename: filename,
+		out:      logFile,
+		logger:   log.New(logFile,"[Debug]",log.LstdFlags),
+	}
+}
+
+func (serverlogger *ServerLogger) Print(prefix string, format string, v ...interface{}) {
+	serverlogger.logger.SetPrefix(prefix)
+	serverlogger.logger.Printf(format, v)
+}
+
+// Close logger
+func (serverlogger *ServerLogger) CloseLogger() {
+	serverlogger.out.Close()
+}
 
 func fatalf(code int, codeStr string, errf string, a ...interface{}) {
 	if DEBUG {
